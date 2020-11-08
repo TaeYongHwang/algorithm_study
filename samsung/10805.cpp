b6590a6c4e818c186784031b0a6fede0fe89b853
taeyonghwang <xodyd15@naver.com>
2020-11-08 23:41:51 +0900

    sansumg 10805 unresulved

--- samsung/10805.cpp
+++ samsung/10805.cpp
@@ -5,89 +5,101 @@
 
 using namespace std;
 
+
 vector<pair<int, int>> mix;
 vector<int> ans;
+char LEFT = 'L';
+char RIGHT = 'R';
+int N;
 
-int LEFT = -1;
-int RIGHT = 1;
-
-void swap(vector<int>& v, int src, int desc) {
-    int temp = src;
-    v[src] = desc;
-    v[desc] = temp;
+bool isDeceivable(int ballLocation, char dir) {
+    if(dir == 'L') {
+        if(ballLocation > 1) return true;
+    } else if(dir == 'R') {
+        if(ballLocation < N) return true;
+    }
+    return false;
 }
 
-int getBallLocation(vector<int>& v) {
-    for(int i = 0 ; i< v.size() ; i++) {
-        if(v[i] == 1) return i;
+int changeBallLocation (int swap1, int swap2, int target) {
+    if(swap1 == target) {
+       return swap2; 
+    } else if(swap2 == target) {
+        return swap1;
     }
+    return target;
 }
 
-int trick(vector<int>& v, int idx, int dir) {
-    if(dir > 0) {
-        v[idx] = 0;
-        v[idx+1] = 1;
-        return idx +1;
-    } else {
-        v[idx] = 0;
-        v[idx-1] = 1;
-        return idx - 1;
-    }
+
+void findAns(int ballLocation = 1, int count = 0, bool isAlreadyDeceived = false) {
+     /** 종료 상황 **/
+     if(count == mix.size()) {
+         /** 속이지 않은 경우 - 1회 속여야 한다. **/
+         if(!isAlreadyDeceived) {
+             if(isDeceivable(ballLocation, LEFT)) {
+                 if(find(ans.begin(), ans.end(), ballLocation -1) == ans.end()) {
+                     ans.push_back(ballLocation -1 );
+                 }
+             }
+             if(isDeceivable(ballLocation, RIGHT)) {
+                 if(find(ans.begin(), ans.end(), ballLocation +1) == ans.end()) {
+                     ans.push_back(ballLocation +1 );
+                 }
+             }
+         } else {
+             /** 이미 속인 경우 **/
+                 if(find(ans.begin(), ans.end(), ballLocation) == ans.end()) {
+                     ans.push_back(ballLocation);
+                 }
+         }
+         return;
+     }
+
+
+     if(!isAlreadyDeceived) {
+         if(isDeceivable(ballLocation, LEFT)) {
+             findAns(changeBallLocation(mix[count].first, mix[count].second, ballLocation - 1), count + 1, true);
+         }
+
+         if(isDeceivable(ballLocation, RIGHT)) {
+             findAns(changeBallLocation(mix[count].first, mix[count].second, ballLocation + 1), count + 1, true);
+         }
+     }
+
+     findAns(changeBallLocation(mix[count].first, mix[count].second, ballLocation), count + 1 , isAlreadyDeceived);
  }
 
-bool isTrick(vector<int>&v, int ballLocation, int dir) {
-    if(dir > 0 && v.size() == ballLocation + 1) return false;
-    if(dir < 0 && ballLocation - 1 == 0) return false;
 
-    return true; 
-}
-
-void find(vector<int>& v, int count = 0, bool useTrick = false) {
-
-    /** 종료 **/ 
-    if(count == mix.size()) {    
-        int ballLocation = getBallLocation(v);
-
-        if(find(ans.begin(), ans.end(), ballLocation) == ans.end()) {
-            ans.push_back(ballLocation);
+void findAns2(int deceivedIdx, char dir, int ballLocation = 1) {
+    for(int i = 0 ; i < mix.size() ; i ++) {
+        if(i == deceivedIdx) {
+            if(dir == RIGHT) {
+                if(!isDeceivable(ballLocation, RIGHT)) return;
+                ballLocation = changeBallLocation(mix[i].first, mix[i].second, ballLocation + 1);
+            } else {
+                if(!isDeceivable(ballLocation, LEFT)) return;
+                ballLocation = changeBallLocation(mix[i].first, mix[i].second, ballLocation - 1);
+            }
+        } else {
+            ballLocation = changeBallLocation(mix[i].first, mix[i].second, ballLocation);
         }
-        return;
-    } 
-
-    // 트릭 안쓴 경우 트릭 사용
-    if(!useTrick) {
-        // 오른쪽 트릭 후 위치 변경
-        int ballLocation = getBallLocation(v);
-        if(isTrick(v, ballLocation, RIGHT)) {
-            ballLocation = trick(v, ballLocation, RIGHT);
-            swap(v, mix[count].first, mix[count].second);
-            find(v, count + 1, true);
-            swap(v, mix[count].second, mix[count].first);
-            ballLocation = trick(v, ballLocation, LEFT);
-        }
-
-        if(isTrick(v, ballLocation, LEFT)) {    
-            // 왼쪽 트릭 후 위치변경
-            ballLocation = trick(v, ballLocation, LEFT);
-            swap(v, mix[count].first, mix[count].second);
-            find(v, count + 1, false);
-            swap(v, mix[count].second, mix[count].first);
-            ballLocation = trick(v, ballLocation, RIGHT);
-
-        }
-
     }
-    
-    // 트릭 안쓰고 변경
-    swap(v, mix[count].first, mix[count].second);
-    find(v, count + 1, useTrick);
-    swap(v, mix[count].first, mix[count].second);
 
+    if(deceivedIdx == mix.size()) {
+        if(dir == RIGHT) {
+            if(!isDeceivable(ballLocation, RIGHT)) return;
+            ballLocation += 1;
+        } else {
+            if(!isDeceivable(ballLocation, LEFT)) return;
+            ballLocation -= 1;
+        }
+    }
+
+     if(find(ans.begin(), ans.end(), ballLocation) == ans.end()) {
+        ans.push_back(ballLocation);
+      }
 
 }
-
-
-
 
 
 int main() {
@@ -95,24 +107,35 @@ int main() {
     cin >> tc;
 
     while(count++ < tc) {
-        int N,Q, src, desc;
+        int Q, src, dest;
         cin >> N >> Q;
 
         while(Q--) {
-            cin >> src >> desc;
-            mix.push_back({src, desc});
+            cin >> src >> dest;
+            mix.push_back({src, dest});
+        }
+        
+        //findAns();
+
+        for(int i = 0 ; i <= mix.size(); i++) {
+            findAns2(i, LEFT);
+            findAns2(i, RIGHT);
         }
 
-        vector<int> v;
-        v.assign(N + 1,0);
-        v[1] = 1;
-
-        find(v);
+        /*
+        for(int i = 0 ; i < ans.size(); i++) {
+            cout<<ans[i]<<' ';
+        }
+        cout<<endl;
+        */
 
         cout<<'#'<<count<<' '<<ans.size()<<endl;
+
     
 
         mix.clear();
         ans.clear();
     }
+
+    return 0;
 }
